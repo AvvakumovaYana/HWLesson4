90a2609b1d0553944d470449cdd39fd596601278
Yana Avvakumova <10.07.93t@gmail.com>
2024-04-30 00:03:24 +0300

    Renamed test and added all JUnit5 code!

--- src/test/java/Lesson4Test.java
+++ src/test/java/Lesson4Test.java
@@ -4,10 +4,21 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class Lesson4Test {
     @Test
-    void test(){
+    void shouldFindJUnit5Code(){
         open("https://github.com/selenide/selenide/wiki");
         $("div.markdown-body ul li a[href='/selenide/selenide/wiki/SoftAssertions']").shouldHave(text("Soft assertions"));
         $("div.markdown-body ul li a[href='/selenide/selenide/wiki/SoftAssertions']").click();
-        $("#wiki-body").shouldHave(text("ExtendWith"));
+        $("#wiki-body").shouldHave(text(
+                "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
     }
 }
